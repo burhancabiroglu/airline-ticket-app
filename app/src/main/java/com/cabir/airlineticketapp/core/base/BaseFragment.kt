fdf4167f6024c98d0ec78c01df8594fe4daba155
Burhan Cabiroğlu <burhancabiroglu97@gmail.com>
2022-09-21 23:42:43 +0300

    [DEV][burhan.cabiroglu][implementation project structure process]

---
+++ app/src/main/java/com/cabir/airlineticketapp/core/base/BaseFragment.kt
@@ -0,0 +1,6 @@
+package com.cabir.airlineticketapp.core.base
+
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment: Fragment() {
+}
--- app/src/main/java/com/cabir/airlineticketapp/ui/main/MainActivity.kt
+++ app/src/main/java/com/cabir/airlineticketapp/ui/main/MainActivity.kt
@@ -1,8 +1,11 @@
-package com.cabir.airlineticketapp
+package com.cabir.airlineticketapp.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.cabir.airlineticketapp.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
